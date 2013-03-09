0a72e002386da946f802499b120b0453a6de0546
Andre <andre.landry@gmail.com>
2013-03-09 13:41:06 -0400

    set up fetch task list from api

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,2 +1,18 @@
 class Project < ActiveRecord::Base
+  
+  
+  
+  has_many :tasks
+  
+  def fetch_tasks
+    rest = RestClient::Resource.new(API_URL,API_KEY)
+    
+    response = rest["projects/#{self.asana_id}/tasks"].get
+    
+    response = JSON.parse(response)
+    
+    response['data'].each do |task|
+      self.tasks.new(name: task['name'], asana_id: task['id'])
+    end
+  end
 end
