6251f2fa3a1a1a4dfe28de0d0c6b0819f1bbfa23
Andre <andre.landry@gmail.com>
2013-03-09 09:29:22 -0400

    initial rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AsanaTools::Application.config.secret_key_base = '522fdbc6122cb214be2697a62205396740fb95d848753fe751e99a7c08527f87cc870c3b79702a71ae7d426e7589c9d9986e0e900b9ac84229d3a761ca9fffb6'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+AsanaTools::Application.config.session_store :encrypted_cookie_store, key: '_asana-tools_session'
