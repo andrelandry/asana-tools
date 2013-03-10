72b754a4b728a2d12fae0dd28556cef8b6ebe50d
Andre <andre.landry@gmail.com>
2013-03-10 14:48:27 -0300

    add timeline plugin

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,6 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+//= require jquery.qtip.min
+//= require raphael-min
+//= require chronoline
--- app/assets/javascripts/projects.js
+++ app/assets/javascripts/projects.js
@@ -1,2 +1,18 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+$(document).ready(function(){
+	
+	var events = [
+		{dates: [new Date(2013, 0, 19)],
+		 title: "2011 Season Opener",
+		 attrs: {} // optional
+		},
+		{dates: [new Date(2013, 0, 13)], title: "Spring Training Begins"},
+		{dates: [new Date(2013, 3, 9), new Date(2013, 3, 11)], title: "Atlanta Braves @ Houston Astros"}
+		];
+		
+	var timeline = new Chronoline(document.getElementById('timeline'), events,
+	    {animated: true});
+		    
+});
